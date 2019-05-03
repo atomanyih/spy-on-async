4b35f24f80b0445f5b487367417a8a07461b5d72
pivotal <augzodia@gmail.com>
2019-05-03 13:30:28 -0700

    build 1.2 and add dist to github for posterity

---
+++ dist/index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const createAsyncMock = () => {
+  let resolvePromise, rejectPromise;
+
+  const promise = new Promise((resolve, reject) => {
+    resolvePromise = resolve;
+    rejectPromise = reject;
+  });
+
+  return {
+    resolve(val) {
+      resolvePromise(val);
+
+      return promise;
+    },
+    reject(val) {
+      rejectPromise(val);
+
+      return promise.catch(() => {
+      });
+    },
+    mock: () => promise
+  };
+};
+
+const multiple = (createAsyncMock) => () => {
+  let asyncMocks = [];
+  const mock = (...args) => {
+    const asyncMock = createAsyncMock();
+    asyncMocks = [asyncMock, ...asyncMocks];
+    return asyncMock.mock(...args)
+  };
+  const resolve = (val) => {
+    const asyncMock = asyncMocks.pop();
+
+    return asyncMock.resolve(val);
+  };
+  const reject = (err) => {
+    const asyncMock = asyncMocks.pop();
+
+    return asyncMock.reject(err);
+  };
+
+  return {mock, resolve, reject}
+};
+
+const spyOnAsync = (module, methodName) => {
+  const {mock, resolve, reject} = multiple(createAsyncMock)();
+  jest.spyOn(module, methodName).mockImplementation(mock);
+  module[methodName].mock.resolve = resolve;
+  module[methodName].mock.reject = reject;
+};
+
+exports.createAsyncMock = createAsyncMock;
+exports.spyOnAsync = spyOnAsync;
