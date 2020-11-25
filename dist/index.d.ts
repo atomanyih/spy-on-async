2e774ad53e22125712b108168bfbd2247b8249d8
August Toman-Yih <august.toman.yih@gmail.com>
2020-11-24 16:19:38 -0800

    make types available from index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
+export { AsyncMock, AsyncSpy } from './AsyncMocker';
 export declare const createAsyncMock: <T>() => import("./AsyncMocker").AsyncMock<T>;
 export declare const spyOnAsync: <ReturnType_1>(module: any, methodName: string) => import("./AsyncMocker").AsyncSpy<ReturnType_1>;
 export declare const resetAllPromises: () => void;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import mocker from './AsyncMocker';
 
+export {AsyncMock, AsyncSpy} from './AsyncMocker';
+
 export const createAsyncMock = mocker.createAsyncMock
 export const spyOnAsync = mocker.spyOnAsync
 export const resetAllPromises = mocker.resetAllPromises
