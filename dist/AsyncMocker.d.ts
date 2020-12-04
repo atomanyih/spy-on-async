faa5f3aded6f929c588d2d54e2d2595ff6ef9e32
August Toman-Yih <august.toman.yih@gmail.com>
2020-12-03 16:03:19 -0800

    build for testing prior to version bump

--- dist/AsyncMocker.d.ts
+++ dist/AsyncMocker.d.ts
@@ -1,19 +1,19 @@
 /// <reference types="jest" />
-export interface AsyncMock<T> extends jest.Mock<Promise<T>> {
-    mockResolveNext(val: T): Promise<T>;
-    mockRejectNext(val?: T): Promise<any>;
+export interface AsyncMock<ResolveType = any, ArgsType extends any[] = any> extends Function, jest.Mock<Promise<ResolveType>, ArgsType> {
+    mockResolveNext(val: ResolveType): Promise<ResolveType>;
+    mockRejectNext(val?: any): Promise<any>;
 }
-export interface AsyncSpy<T> extends jest.SpyInstance<Promise<T>> {
-    mockResolveNext(val: T): Promise<T>;
-    mockRejectNext(val?: T): Promise<any>;
+export interface AsyncSpy<ResolveType, ArgsType extends any[] = any> extends jest.SpyInstance<Promise<ResolveType>, ArgsType> {
+    mockResolveNext(val: ResolveType): Promise<ResolveType>;
+    mockRejectNext(val?: any): Promise<any>;
 }
 declare class AsyncMocker {
     private _resetRegistry;
     constructor();
     resetAllPromises: () => void;
-    createAsyncMock: <T>() => AsyncMock<T>;
-    createAsyncMockSingleton: <T>() => AsyncMock<T>;
-    spyOnAsync: <ReturnType_1>(module: any, methodName: string) => AsyncSpy<ReturnType_1>;
+    createAsyncMock: <ResolveType>() => AsyncMock<ResolveType, any>;
+    createAsyncMockSingleton: <ResolveType>() => AsyncMock<ResolveType, any>;
+    spyOnAsync: <ResolveType>(module: any, methodName: string) => AsyncSpy<ResolveType, any>;
 }
 declare const mocker: AsyncMocker;
 export default mocker;
